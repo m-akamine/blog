5228cfdbc667632d4417058a3160d439b49ad97a
m-akamine <m-akamine@it-college.ac.jp>
2023-12-15 02:56:15 +0000

    2.6 End
    _app.jsにLayoutを追加し、各ページの簡略化

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import Layout from 'components/layout'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
+export default MyApp
