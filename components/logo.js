eeb269d3c1ec01bccb7771bcc5ebaa43a91f990b
m-akamine <m-akamine@it-college.ac.jp>
2023-12-15 07:36:36 +0000

    3.6 End P92-P95
    logo.jsにlogo.module.cssをリンク

--- components/logo.js
+++ components/logo.js
@@ -1,7 +1,12 @@
 import Link from 'next/link'
+import styles from 'styles/logo.module.css'
 
-const Logo = () => {
-  return <Link href='/'>CUBE</Link>
+const Logo = ({ boxOn = false }) => {
+  return (
+    <Link href='/' className={boxOn ? styles.box : styles.basic}>
+      CUBE
+    </Link>
+  )
 }
 
 export default Logo
