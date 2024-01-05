9e45a9c9a82d9be535b51b1be56346a749189282
m-akamine <m-akamine@it-college.ac.jp>
2024-01-05 09:34:05 +0000

    4.7 End P125-P127
    Contactコンポーネントでコンタクト情報を管理する

---
+++ components/contact.js
@@ -0,0 +1,12 @@
+import styles from 'styles/contact.module.css'
+
+const Contact = () => {
+  return (
+    <div className={styles.stack}>
+      <h3 className={styles.heading}>Contact</h3>
+      <address>cube@web.mail.address</address>
+    </div>
+  )
+}
+
+export default Contact
