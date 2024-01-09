3f767c8e84b8a0fea0d29704c172562c788826a1
m-akamine <m-akamine@it-college.ac.jp>
2024-01-09 12:52:08 +0000

    5.8 End P172-P177
    Font Awesomeの基本的な使い方

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,12 @@ import Layout from 'components/layout'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHouseChimney } from '@fortawesome/free-solid-svg-icons'
+import { faSun } from '@fortawesome/free-regular-svg-icons'
+import { faTwitter, faFacebookF } from '@fortawesome/free-brands-svg-icons'
 
+library.add(faHouseChimney, faSun, faTwitter, faFacebookF)
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
