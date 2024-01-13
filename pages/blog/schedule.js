5513625ca259d0a8704ebb96274dd4b960e60dd0
m-akamine <m-akamine@it-college.ac.jp>
2024-01-13 08:17:23 +0000

    8.4 End P234-P239
    記事の本文を表示する

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,13 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar
+} from 'components/two-column'
+import ConvertBody from 'components/convert-body'
 import Image from 'next/image'
 
 const Schedule = ({ title, publish, content, eyecatch, categories }) => {
@@ -22,6 +29,14 @@ const Schedule = ({ title, publish, content, eyecatch, categories }) => {
             }}
           />
         </figure>
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar></TwoColumnSidebar>
+        </TwoColumn>
       </article>
     </Container>
   )
