7459190f00648993e823c73a41d67242912e983a
m-akamine <m-akamine@it-college.ac.jp>
2024-01-18 03:14:15 +0000

    9.5 End P268-P271
    記事一覧を作成する

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,15 +1,29 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
 
 const props = { title: 'Blog', subtitle: 'Recent Posts' }
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
     <Container>
-      <Meta pageTitle='ブログ' pageDesc="ブログの記事一覧" />
+      <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
       <Hero {...props} />
+
+      <Posts posts={posts} />
     </Container>
   )
 }
+
+const getStaticProps = async () => {
+  const posts = await getAllPosts()
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
 export default Blog
+export { getStaticProps }
