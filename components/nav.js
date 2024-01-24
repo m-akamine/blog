ffe7120690da42d6d57b18cfc65f950b1d3a371f
m-akamine <m-akamine@it-college.ac.jp>
2024-01-24 03:08:27 +0000

    10.3 End P295-P309
    useStateを使ってハンバーガーメニューを作成する

--- components/nav.js
+++ components/nav.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import styles from 'styles/nav.module.css'
 
@@ -8,12 +9,39 @@ const NavList = [
 ]
 
 const Nav = () => {
+  const [navIsOpen, setNavIsOpen] = useState(false)
+
+  const toggleNav = () => {
+    setNavIsOpen(prev => !prev)
+  }
+
+  const closeNav = () => {
+    setNavIsOpen(false)
+  }
+
   return (
-    <nav>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      {navIsOpen && (
+        <style jsx global>{`
+          @media (max-width: 767px) {
+            body {
+              overflow: hidden;
+              position: fixed;
+              width: 100%;
+            }
+          }
+        `}</style>
+      )}
+      <button className={styles.btn} onClick={toggleNav}>
+        <span className={styles.bar}></span>
+        <span className='sr-only'>MENU</span>
+      </button>
       <ul className={styles.list}>
         {NavList.map(post => (
           <li key={post.title}>
-            <Link href={post.url}>{post.title}</Link>
+            <Link href={post.url} onClick={closeNav}>
+              {post.title}
+            </Link>
           </li>
         ))}
       </ul>
