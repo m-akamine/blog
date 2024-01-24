043218bf93a833990416bd1444b0c26b08c7a076
m-akamine <m-akamine@it-college.ac.jp>
2024-01-24 02:25:27 +0000

    9.10 End P287-P289
    カテゴリーページに記事一覧を表示する

--- pages/blog/category/[slug].js
+++ pages/blog/category/[slug].js
@@ -1,11 +1,20 @@
-import { getAllCategories } from 'lib/api'
+import { getAllCategories, getAllPostsByCategory } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
 
-const Category = ({ name }) => {
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+// plaiceholder3用の追加コード
+import { getImageBuffer } from 'lib/getImageBuffer'
+
+const Category = ({ name, posts }) => {
   return (
     <Container>
       <PostHeader title={name} subtitle='Blog Category' />
+      <Posts posts={posts} />
     </Container>
   )
 }
@@ -24,9 +33,20 @@ const getStaticProps = async context => {
   const allCats = await getAllCategories()
   const cat = allCats.find(({ slug }) => slug === catSlug)
 
+  const posts = await getAllPostsByCategory(cat.id)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const imageBuffer = await getImageBuffer(post.eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    post.eyecatch.blurDataURL = base64
+  }
   return {
     props: {
-      name: cat.name
+      name: cat.name,
+      posts: posts
     }
   }
 }
