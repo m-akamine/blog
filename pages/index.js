b0136cae8db3c395594440404ac52660a83ee0fd
m-akamine <m-akamine@it-college.ac.jp>
2024-01-24 01:10:47 +0000

    9.7 End P276-P277
    トップページに記事一覧を表示する

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
-import Meta from 'components/meta'
+import { getAllPosts } from 'lib/api'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+// plaiceholder3 対応用
+import { getImageBuffer } from 'lib/getImageBuffer'
 
 const props = {
   title: 'CUBE',
@@ -8,12 +17,35 @@ const props = {
   imageOn: true
 }
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <Container>
-      <Meta />
       <Hero {...props} />
+
+      <Posts posts={posts} />
+      <Pagination nexUrl='/blog' nextText='More Posts' />
     </Container>
   )
 }
+
+const getStaticProps = async () => {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const imageBuffer = await getImageBuffer(post.eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
+
 export default Home
+export { getStaticProps }
